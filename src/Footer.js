778bdb74f554facb12f036394da65197ff1289b4
justin <justincoh@gmail.com>
2024-01-25 09:23:24 -0500

    update footer link

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import logo from "./justin_joyce_logo.png"
 const Footer = () => {
     return (
         <p className="text-center">
-            <a href="https://justinjoyce.dev/">
+            <a href="https://justinjoyce.dev/meeting-cost-calculator">
                 <img src={logo} width="50" alt="justin joyce logo"></img>
             </a>
         </p>
